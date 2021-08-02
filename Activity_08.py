088d43871c8f4cbbe07f93751b231a79c581f475
Priya Jasmin <priyar.ec19@bmsce.ac.in>
2021-08-02 16:33:52 +0530

    initial commit

---
+++ Activity_08.py
@@ -0,0 +1,3 @@
+1) An interpreted language is one which converts lines of code instantaneously to machine code and executes it. 
+2) While an interpreter converts code line by line to machine code and executes it, a compiler converts the entire code to machine code making it ready for execution.
+3) C

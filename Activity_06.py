3c71445f424cc769ad37f78d7d6cdb853644000a
Priya Jasmin <priyar.ec19@bmsce.ac.in>
2021-08-03 00:23:09 +0530

    actual initial commit

--- Activity_06.py
+++ Activity_06.py
@@ -1,7 +1,12 @@
-inp = input("Enter the numbers to be added separated by spaces :  ")
-operands = inp.split(' ')
-sum = 0.0
-for i in operands:
-	sum += int(i)
-	print (i + ' + ', end='')
-print (' = %.3f' %sum)
+inp = input("Enter 5 numbers :  ")
+number = inp.split(' ')
+for i in range(0, 5):
+	number[i] = int(number[i])
+number3 = number[0:3]
+print("sliced list = ",number3)
+number[0]=0
+number[4]=0
+number3[0]=0
+number3[2]=0
+print("replaced list-1 = ",number)
+print("replaced list-2 = ",number3)
